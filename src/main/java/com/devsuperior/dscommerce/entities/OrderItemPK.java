cce86fd285e461bc1a843ee27dfb1dc55f4859ed
EduardoGomes1982 <edu1982talk@gmail.com>
2024-09-19 23:11:05 -0300

    update: equals and hashcode

--- src/main/java/com/devsuperior/dscommerce/entities/OrderItemPK.java
+++ src/main/java/com/devsuperior/dscommerce/entities/OrderItemPK.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -11,10 +12,12 @@ import lombok.NoArgsConstructor;
 @Embeddable
 public class OrderItemPK {
 
+    @EqualsAndHashCode.Include
     @ManyToOne
     @JoinColumn(name = "order_id")
     private Order order;
 
+    @EqualsAndHashCode.Include
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
